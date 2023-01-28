aa614db30d2ce92773be3e02ece08120f98ead71
Illiasse RIFKI <i.rifki@oneprog.fr>
2023-01-28 15:51:02 +0100

    feat: add small button

--- fameway-admin/src/pages/dashboard/Dashboard.js
+++ fameway-admin/src/pages/dashboard/Dashboard.js
@@ -5,6 +5,7 @@ import Breadcrumb from "../../layouts/full-layout/breadcrumb/Breadcrumb";
 import PageContainer from "../../components/container/PageContainer";
 import { userAtom } from "../../atoms/Atoms";
 import { useAtom } from "jotai";
+import FeatherIcon from "feather-icons-react";
 
 import {
   EarningsShop,
@@ -18,6 +19,8 @@ import {
   MedicalproBranding,
   BlogCard,
 } from "../dashboard2-components";
+import { Button, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 
 const Dashboard1 = () => {
   const [currentUser] = useAtom(userAtom);
@@ -27,11 +30,45 @@ const Dashboard1 = () => {
       title="Fameway - Gestion de boutique"
       description="Main informations"
     >
-      <Breadcrumb
-        title={`Voilà ce qu’il s’est passé depuis votre dernière connexion`}
-        subtitle={`Bonjour, `}
-        username={currentUser?.username}
-      ></Breadcrumb>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          width: "100%",
+          pl: 1,
+          alignItems: "flex-end",
+        }}
+      >
+        <Breadcrumb
+          title={`Voilà ce qu’il s’est passé depuis votre dernière connexion`}
+          subtitle={`Bonjour, `}
+          username={currentUser?.username}
+        ></Breadcrumb>
+
+        <Button
+          // onClick={handleStoreUpdate}
+          color="primary"
+          variant="contained"
+          sx={{
+            height: "40px",
+            width: "300px",
+            fontWeight: "700",
+            borderRadius: "100px",
+          }}
+        >
+          <FeatherIcon icon="plus" width="20" />
+          <Typography
+            variant="button"
+            color="white"
+            fontWeight={600}
+            sx={{
+              marginLeft: 1,
+            }}
+          >
+            Créer un nouvel article
+          </Typography>
+        </Button>
+      </Box>
     </PageContainer>
   );
 };
